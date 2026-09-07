4dc72b203e768de3daf340ec1144cc1d972fd739
StephanyT <46168485@certus.edu.pe>
2026-09-07 00:37:10 -0500

    fe-update

--- sql/agregar_password.sql
+++
@@ -1,5 +0,0 @@
--- Ya tienes la tabla docentes creada sin columna de contrasena.
--- Corre esto UNA VEZ en phpMyAdmin (pestana "SQL") para agregarla,
--- en vez de volver a crear la tabla desde cero.
-
-ALTER TABLE docentes ADD COLUMN password VARCHAR(255) NOT NULL AFTER correo;
